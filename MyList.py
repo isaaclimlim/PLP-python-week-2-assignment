aa76568654611772fa3f0e18172c1199a794ea5f
isaaclimlim <isaacerukudi@gmail.com>
2024-11-26 19:20:15 +0300

    The assignment is completed

---
+++ MyList.py
@@ -0,0 +1,19 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list[1] = 15
+
+my_list.extend([50,60,70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_30 = my_list.index(30)
+print(index_30)
+
+print(f"My final list is: {my_list}")
